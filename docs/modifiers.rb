0842ed527bdc426cf9f7df5eb1d07959d197231e
alexeypetrushin <alexey.petrushin@gmail.com>
2011-09-16 02:56:58 +0400

    upd docs, add databases

---
+++ docs/modifiers.rb
@@ -0,0 +1,38 @@
+# Modeifiers in [Mongo Model][mongodb_model].
+#
+# Usually if You want to update model You load it, make changes
+# and save it back. But if You want to update only some of attributes
+# there's more efficient way - modifiers.
+#
+# In this example we'll create simpel model and update it using modifiers.
+require 'mongo/model'
+
+# Connecting to test database and cleaning it before starting.
+Mongo::Model.default_database_name = :default_test
+Mongo::Model.default_database.clear
+
+# Defining game Unit.
+class Unit
+  inherit Mongo::Model
+  collection :units
+
+  attr_accessor :name, :life
+end
+
+# Creating mighty Tassadar.
+zeratul = Unit.create name: 'Tassadar', life: 80
+
+# Updating model with modifiers.
+zeratul.update _inc: {life: -40}
+zeratul.reload
+p zeratul.life                                   # => 40
+
+# There's also helper on the model class.
+Unit.update({_id: zeratul._id}, {_inc: {life: -20}})
+zeratul.reload
+p zeratul.life                                   # => 20
+
+# In this example we covered using modifiers.
+#
+# [mongodb_model]:     index.html
+# [composite]:         composite.html
