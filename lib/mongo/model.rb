c54bf9b1c20e57925b6a20d696416994ffca160d
alexeypetrushin <alexey.petrushin@gmail.com>
2014-09-15 00:30:21 +0400

    New docs.

--- lib/mongo/model.rb
+++
@@ -1,8 +0,0 @@
-require 'mongodb_model/gems'
-
-require 'mongo/object'
-
-module Mongo
-  autoload :Model, 'mongo/model/load'
-end
-
--- lib/mongodb_model/gems.rb
+++
@@ -1,5 +0,0 @@
-if respond_to? :fake_gem
-  fake_gem 'mongodb'
-  fake_gem 'validatable2'
-  fake_gem 'ruby_ext'
-end
